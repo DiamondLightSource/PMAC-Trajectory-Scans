d5c686259507cdee4d7bced048b9b8b08060c028
Gary Yendell <gary.yendell@diamond.ac.uk>
2016-03-18 15:05:12 +0000

    Add scanpointgenerator for travis

---
+++ scanpointgenerator/linegenerator.py
@@ -0,0 +1,37 @@
+from scanpointgenerator import ScanPointGenerator
+from point import Point
+
+
+class LineGenerator(ScanPointGenerator):
+    """Generate equally spaced scan points in one dimension"""
+
+    def __init__(self, name, units, start, step, num):
+        """Initialise the generator
+
+        Args:
+            name (str): The scannable name. E.g. "x"
+            units (str): The scannable units. E.g. "mm"
+            start (float): The first position to be generated. E.g. 1.5
+            step (float): The increment for each successive position. E.g. 0.5
+            num (int): The number of points to generate. E.g. 5
+        """
+        self.name = name
+        self.start = start
+        self.step = step
+        self.num = num
+        self.position_units = {name: units}
+        self.index_dims = [self.num]
+        self.index_names = [name]
+
+    def _calc(self, i):
+        """Calculate the position for a given index"""
+        return self.start + i * self.step
+
+    def iterator(self):
+        for i in xrange(self.num):
+            point = Point()
+            point.positions[self.name] = self._calc(i)
+            point.lower[self.name] = self._calc(i - 0.5)
+            point.upper[self.name] = self._calc(i + 0.5)
+            point.indexes = [i]
+            yield point
---
+++ scanpointgenerator/nestedgenerator.py
@@ -0,0 +1,49 @@
+from scanpointgenerator import ScanPointGenerator
+from point import Point
+
+class NestedGenerator(ScanPointGenerator):
+    """Nest two generators, optionally alternating each row of the inner"""
+
+    def __init__(self, outer, inner, snake=False):
+        """Initialise the generator
+
+        Args:
+            outer (ScanPointGenerator): The slower generator
+            inner (ScanPointGenerator): The faster generator
+            snake (bool): If True, alternate odd rows of the inner generator
+        """
+        self.outer = outer
+        self.inner = inner
+        self.snake = snake
+        self.position_units = outer.position_units.copy()
+        self.position_units.update(inner.position_units)
+        self.index_dims = outer.index_dims + inner.index_dims
+        self.index_names = outer.index_names + inner.index_names
+
+    def iterator(self):
+        for i, outer in enumerate(self.outer.iterator()):
+            inner_iterator = self.inner.iterator()
+            alternate = False
+            if self.snake and i % 2:
+                alternate = True
+                # Reverse the inner iterator as in place list
+                inner_iterator = list(inner_iterator)
+                inner_iterator.reverse()
+            for inner in inner_iterator:
+                point = Point()
+                # Insert outer points
+                point.positions.update(outer.positions)
+                point.lower.update(outer.positions)
+                point.upper.update(outer.positions)
+                # Insert inner points
+                point.positions.update(inner.positions)
+                # alternate has lower and upper bound swapped
+                if alternate:
+                    point.upper.update(inner.lower)
+                    point.lower.update(inner.upper)
+                else:
+                    point.upper.update(inner.upper)
+                    point.lower.update(inner.lower)
+                # Insert indexes
+                point.indexes = outer.indexes + inner.indexes
+                yield point
